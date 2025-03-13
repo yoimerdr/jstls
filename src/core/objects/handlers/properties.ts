51a691472c4c6d82d12b108a35d67f3e3c6f28ab
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-03-13 17:49:05 -0500

    feat: add handlers for get specific function/non-function keys from object

--- src/core/objects/handlers/properties.ts
+++ src/core/objects/handlers/properties.ts
@@ -1,4 +1,9 @@
-import {Keys} from "../../../types/core";
+import {Keys, MethodKeys, PropertiesKeys} from "../../../types/core";
+import {call} from "../../functions/call";
+import {isFunction} from "../types";
+import {includes} from "../../polyfills/indexable/es2016";
+import {filter} from "../../iterable/filter";
+import {nreturns} from "../../utils";
 
 interface KeysShortcut {
   <T>(object: T): Keys<T>[];
@@ -22,3 +27,40 @@ export const keys: KeysShortcut = Object.keys;
  * @see {Object.getOwnPropertyNames}
  */
 export const propertyNames: KeysShortcut = Object.getOwnPropertyNames;
+
+
+export const commonStatics = ['length', 'name', 'prototype',];
+
+export const commonPrototype = ["constructor"];
+
+function filterFromObject<T>(keys: (object: T) => Keys<T>[], condition: (value: any) => boolean, object: T, mode: 'statics' | 'prototype'): Keys<T>[] {
+  const common = mode === 'statics' ? commonStatics :
+    (mode === 'prototype' ? commonPrototype : null)
+  return filter(keys(object), key => {
+    return (!common || !call(includes, common, key)) && condition(object[key])
+  });
+}
+
+export function simpleKeys<T>(object: T): PropertiesKeys<T>[];
+export function simpleKeys<T>(object: T, filter: 'statics' | 'prototype'): PropertiesKeys<T>[];
+export function simpleKeys<T>(object: T, mode?: 'statics' | 'prototype'): PropertiesKeys<T>[] {
+  return filterFromObject(keys, nreturns(isFunction), object, mode!) as PropertiesKeys<T>[];
+}
+
+export function methodKeys<T>(object: T): MethodKeys<T>[];
+export function methodKeys<T>(object: T, filter: 'statics' | 'prototype'): MethodKeys<T>[];
+export function methodKeys<T>(object: T, mode?: 'statics' | 'prototype'): MethodKeys<T>[] {
+  return filterFromObject(keys, isFunction, object, mode!) as MethodKeys<T>[];
+}
+
+export function simpleProperties<T>(object: T): MethodKeys<T>[];
+export function simpleProperties<T>(object: T, filter: 'statics' | 'prototype'): MethodKeys<T>[];
+export function simpleProperties<T>(object: T, mode?: 'statics' | 'prototype'): MethodKeys<T>[] {
+  return filterFromObject(propertyNames, nreturns(isFunction), object, mode!) as MethodKeys<T>[];
+}
+
+export function methodProperties<T>(object: T): MethodKeys<T>[];
+export function methodProperties<T>(object: T, filter: 'statics' | 'prototype'): MethodKeys<T>[];
+export function methodProperties<T>(object: T, mode?: 'statics' | 'prototype'): MethodKeys<T>[] {
+  return filterFromObject(propertyNames, isFunction, object, mode!) as MethodKeys<T>[];
+}
